d5f579846d6ee3e968921cff28342f7e60deeccc
ChoCoBen <72436348+ChoCoBen@users.noreply.github.com>
2022-06-20 15:46:37 -0400

    Add files via upload

---
+++ src/scripts/viz1/viz1.js
@@ -0,0 +1,9 @@
+/**
+ * Build the third vizualisation
+ *
+ * @param {*} g the d3 selection of the graph
+ */
+export function build (g) {
+  g.selectAll('*').remove()
+  g.append('text').text('AAA')
+}
