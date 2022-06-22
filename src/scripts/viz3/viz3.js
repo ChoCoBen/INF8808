f9a2f2569735dd57f207092b33f7defb92a98244
ChoCoBen <corentin.hubert@imt-atlantique.net>
2022-06-22 16:06:11 -0400

    avancement visualisation 3 + modification des données

--- src/scripts/viz3/viz3.js
+++ src/scripts/viz3/viz3.js
@@ -1,3 +1,6 @@
+import * as preprocess from './preprocess3.js'
+import * as axis from './axis3.js'
+
 /**
  * Build the third vizualisation
  *
@@ -5,5 +8,17 @@
  */
 export function build (g) {
   g.selectAll('*').remove()
+  d3.csv('./par_profession_2019.csv').then((data2019) => {
+    d3.csv('./par_profession_2021.csv').then((data2021) => {
+      data2019 = preprocess.filterRegion(data2019)
+      data2021 = preprocess.filterRegion(data2021)
+
+      var skills = preprocess.getSkills(data2019)
+      data2019 = preprocess.structureData(data2019, skills)
+      data2021 = preprocess.structureData(data2021, skills)
+
+      axis.appendAxes(g)
+    })
+  })
   g.append('text').text('CCCCC')
 }
