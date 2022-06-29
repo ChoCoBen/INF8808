6818a51756d0bf1d28bf376d12e1ba2965c11d69
ChoCoBen <corentin.hubert@imt-atlantique.net>
2022-06-29 18:07:32 -0400

    ~~~~~~~~

--- docs/scripts/viz3/viz3.js
+++ docs/scripts/viz3/viz3.js
@@ -6,7 +6,7 @@ import * as tooltip from './tooltip.js'
 import * as legend from './legend.js'
 import * as helper from '../helper.js'
 
-import d3Tip from '../../node_modules/d3-tip/index.js'
+import d3Tip from '~/node_modules/d3-tip/index.js'
 
 /**
  * Build the third vizualisation
